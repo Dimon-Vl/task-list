56a6d6b7a033ecf7380c1cf8b6448033ee50e9e8
Dmytro <dimapolishchuk15@gmail.com>
2025-08-27 21:21:26 +0300

    add blue theme

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -3,16 +3,17 @@ import { createContext, useState, useEffect } from "react"
 export const ThemeContext = createContext()
 
 export const ThemeProvider = ({ children }) => {
-    const [theme, setTheme] = useState("light")
+    const themeList = ['light', 'blue', 'dark']
+    const [theme, setTheme] = useState(themeList[0])
 
     useEffect(() => {
         document.documentElement.setAttribute("data-theme", theme)
     }, [theme])
 
     const toggleTheme = () => {
-        setTheme((prev) => (prev === "light" ? "dark" : "light"))
+        setTheme((prev) => themeList[(themeList.indexOf(prev) + 1) % themeList.length])
     }
-    
+
     return (
         <ThemeContext.Provider value={{ theme, toggleTheme }}>
             {children}
